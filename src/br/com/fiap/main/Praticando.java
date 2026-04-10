cf5b55bda9e30a97a5cf56c90f859fcd7d8ab683
JVPR <jvprocha.05@gmail.com>
2026-04-10 10:07:19 -0300

    Corrigindo praticando1

--- src/br/com/fiap/main/Praticando.java
+++ src/br/com/fiap/main/Praticando.java
@@ -1,32 +1,36 @@
 package br.com.fiap.main;
 
-import javax.swing.*;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Scanner;
 
 public class Praticando {
     public static void main(String[] args) {
-        //Exercício.1
+
         String frase, novapalavra, substituida, frase2;
         Scanner scan = new Scanner(System.in);
+
         try {
             System.out.println("Digite uma frase:");
             frase = scan.nextLine();
+
             System.out.println("Frase: " + frase);
-            System.out.println("Quantidade de caracteres da frase:" + frase.length());
-            System.out.println("Frase com tudo maiusculo:" + frase.toUpperCase());
-            System.out.println("Digite duas palavras: Uma que a frase possui e outra que deseja substituir pela primeira palavra.");
-            substituida = scan.next();
-            novapalavra = scan.next();
-            System.out.println(frase.replace(substituida,novapalavra));
-            frase = scan.nextLine();
-            frase2 = frase.replace(substituida,novapalavra);
-            System.out.println(frase);
-            System.out.println("Quantidade de caracteres da nova frase:" + frase2.length());
+            System.out.println("Quantidade de caracteres da frase: " + frase.length());
+            System.out.println("Frase com tudo maiúsculo: " + frase.toUpperCase());
+
+            System.out.println("Digite a palavra que deseja substituir:");
+            substituida = scan.nextLine();
+
+            System.out.println("Digite a nova palavra:");
+            novapalavra = scan.nextLine();
+
+            frase2 = frase.replace(substituida, novapalavra);
+
+            System.out.println("Frase atualizada: " + frase2);
+            System.out.println("Quantidade de caracteres da nova frase: " + frase2.length());
+
         } catch (Exception e) {
             System.out.println("ERRO: Informações inválidas!");
         }
 
+        scan.close();
     }
 }
